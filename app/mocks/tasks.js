12288b1660bd86cc9cadeeeeba7eb4931e8ad31c
Uladzimir Dziomin <uladzimir.dziomin@gmail.com>
2017-12-12 14:12:58 +0300

    Update mock

--- app/mocks/tasks.js
+++ app/mocks/tasks.js
@@ -1,36 +1,12 @@
 /* eslint-disable max-len */
+/* eslint-disable no-useless-escape */
 
 const tasks = [
     {
         name: 'Closed Circuit',
         description: 'Create a function that check closed or nor circuit in provided \'area\'. Provided \'area\' - Martix (n*m) that must contain only 0 and 1 (0 - empty, 1 - circuit link). Links(1) can connect diagonally (45 degrees)',
         language: 'javascript',
-        test: `
-            describe("top", function(){
-                let area = [[0,0,0,1,1,0,0,0],
-                            [0,0,0,1,0,1,0,0],
-                            [0,0,0,1,0,1,0,0],
-                            [0,0,0,0,0,0,0,0]];
-
-
-                it('should return false if circuit is'nt closed', () => {
-                    assert.equal(isCircuitClosed(area), false);
-                });
-
-                it('should return true if circuit closed', () => {
-                    area[2][4] = 1;
-
-                    assert.equal(isCircuitClosed(area), true);
-                });
-
-                it('should return false if only one link in circuit', () => {
-                    area = [[0,0,0],
-                            [0,1,0],
-                            [0,0,0]];
-
-                    assert.equal(isCircuitClosed(area), false);
-                });
-                {}
+        test: `describe(\"top\", function(){let area = [[0,0,0,1,1,0,0,0], [0,0,0,1,0,1,0,0], [0,0,0,1,0,1,0,0], [0,0,0,0,0,0,0,0]]; it(\"should return false if circuit isnt closed\", function () { Test.assertEquals(isCircuitClosed(area), false); }); it(\"should return true if circuit closed\", () => { area[2][4] = 1; Test.assertEquals(isCircuitClosed(area), true); }); it(\"should return false if only one link in circuit\", function() { area = [[0,0,0], [0,1,0], [0,0,0]]; Test.assertEquals(isCircuitClosed(area), false); }) });
         `,
         tests: [
             'let area = [[0,0,0,1,1,0,0,0],\\n\' +\n' +
