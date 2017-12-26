1a0a5eb79c1202fbbc7675bb0cd77f57645a72e7
Uladzislau Charniauski <Uladzislau_Charniauski@epam.com>
2017-12-26 15:36:23 +0300

    fix login with github

--- app/routers/auth.router.js
+++ app/routers/auth.router.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import passport from 'passport';
+import config from '../configuration';
 
 /**
  * @module Routes/Auth route
@@ -8,10 +9,13 @@ import passport from 'passport';
 
 const router = express.Router();
 
+const successRedirect = config.get('github:login:success_redirect');
+const failureRedirect = config.get('github:login:failure_redirect');
+
 router.get('/auth/github', passport.authenticate('github'));
 router.get('/auth/github/callback',
-    passport.authenticate('github', {failureRedirect: '/login'}),
-    (req, res) => res.redirect('/')
+    passport.authenticate('github', {failureRedirect}),
+    (req, res) => res.redirect(successRedirect)
 );
 
 
