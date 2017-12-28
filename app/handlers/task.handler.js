71cc82361b4b04adad7185ca2fbefc1468a835e5
Uladzislau Charniauski <Uladzislau_Charniauski@epam.com>
2017-12-28 16:07:04 +0300

    add new fields to task model

--- app/handlers/task.handler.js
+++ app/handlers/task.handler.js
@@ -3,7 +3,7 @@ import * as commonCrudOperations from '../libs/commonCrudOperations';
 
 export const getAll = commonCrudOperations.getAll(Task);
 
-export const getById = commonCrudOperations.getById(Task);
+export const getById = commonCrudOperations.getById(Task, 'id');
 
 export const add = commonCrudOperations.add(Task);
 
