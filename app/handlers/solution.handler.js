fbc3fb286ad6f04c67a50bd4ae6694ea36ba8d6b
Uladzimir Dziomin <uladzimir.dziomin@gmail.com>
2018-01-20 17:02:04 +0300

    Fix solution by task id get

--- app/handlers/solution.handler.js
+++ app/handlers/solution.handler.js
@@ -1,9 +1,11 @@
 import * as commonCrudOperations from '../libs/commonCrudOperations';
 import Solution from '../models/solution';
 import SolutionService from '../services/solution';
+import TaskService from '../services/task';
 import RunnerService from '../services/runner';
 
 const solutionService = new SolutionService(new RunnerService);
+const taskService = new TaskService();
 
 export const getAll = commonCrudOperations.getAll(Solution);
 export const getById = commonCrudOperations.getById(Solution);
@@ -25,9 +27,15 @@ export function sendTask(req, res, next) {
 export function getByTaskId(req, res, next) {
   const {user} = req;
   if (!user) return next('User missed');
-  const {taskId} = req.body;
-  solutionService
-    .getByTaskId(taskId, user._id)
+  const taskId = req.params.id;
+  getSolutionByTaskId(taskId, user._id)
     .then((data) => res.json(data))
     .catch(next);
+}
+
+async function getSolutionByTaskId(taskId, userId) {
+  const task = await taskService.getTaskById(taskId);
+
+  return solutionService
+    .getByTaskId(task._id, userId);
 }
